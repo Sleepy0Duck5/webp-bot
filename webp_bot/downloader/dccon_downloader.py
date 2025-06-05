a87910522bacc3fad5275d93dd1e258234b5794f
sleepy0duck5 <sleepy0duck5@gmail.com>
2025-06-05 17:06:56 +0900

    chore: supports dccon

--- webp_bot/downloader/dccon_downloader.py
+++ webp_bot/downloader/dccon_downloader.py
@@ -1,5 +1,5 @@
 import tempfile
-import requests
+import urllib
 
 from webp_bot.downloader.downloader import Downloader
 from webp_bot.constants import DCinside
@@ -8,20 +8,10 @@ from webp_bot.constants import DCinside
 class DcconDownloader(Downloader):
     def _download_file(self, url: str) -> tempfile._TemporaryFileWrapper:
         with tempfile.NamedTemporaryFile(delete=False) as f:
-            session = requests.Session()
-            dccon_img_request = session.post(
-                url,
-                headers={"X-Requested-With": "XMLHttpRequest", "type": "image"},
-                # headers={
-                #     "accept": "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
-                #     "referer": DCinside.REFERER,
-                #     "host": "dcimg5.dcinside.com",
-                #     "Sec-Fetch-Dest": "image",
-                #     "Sec-Fetch-Mode": "no-cors",
-                #     "Sec-Fetch-Site": "same-site",
-                # },
-            )
+            con_url = urllib.request.Request(url)
+            con_url.add_header("Referer", DCinside.REFERER)
+            con_file = urllib.request.urlopen(con_url)
 
-            f.write(dccon_img_request.content)
+            f.write(con_file.read())
             f.flush()
             return f
